691a43d5b867d74cc62368a590f8a8e8bd31d991
johanfortus <fortusjohan@gmail.com>
2024-11-07 14:59:52 -0500

    added inputs for date range, country, engagement, and tags

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 
-let algoTitle = document.querySelector(".algorithm-title")
-let algoDesc = document.querySelector(".algorithm-desc")
-let tabOne = document.querySelector(".tab-one")
+let algoTitle = document.querySelector(".algorithm-title");
+let algoDesc = document.querySelector(".algorithm-desc");
 
-let tabTwo = document.querySelector(".tab-two")
+let tabOne = document.querySelector(".tab-one");
+let tabTwo = document.querySelector(".tab-two");
+
+let tagInput = document.querySelector(".tag-input");
 
 tabOne.addEventListener("click", (e) => {
     e.preventDefault();
     if(tabOne.classList[1] !== "active") {
-        tabOne.classList.toggle("active")
-        tabTwo.classList.toggle("active")
-        algoTitle.innerText = "K-3 Means Search Algorithm"
+        tabOne.classList.toggle("active");
+        tabTwo.classList.toggle("active");
+        algoTitle.innerText = "K-3 Means Search Algorithm";
         algoDesc.innerText = "k-means clustering is a method of vector quantization, originally from signal processing, that aims to partition n observations into k clusters";
     }
 })
@@ -18,11 +20,10 @@ tabOne.addEventListener("click", (e) => {
 tabTwo.addEventListener("click", (e) => {
     e.preventDefault();
     if(tabTwo.classList[1] !== "active") {
-        tabTwo.classList.toggle("active")
-        tabOne.classList.toggle("active")
-        algoTitle.innerText = "Apriori Algorithm"
+        tabTwo.classList.toggle("active");
+        tabOne.classList.toggle("active");
+        algoTitle.innerText = "Apriori Algorithm";
         algoDesc.innerText = "A machine learning algorithm to identify relationships between items by identifying frequent itemsets.";
     }
-        
-    
 })
+
