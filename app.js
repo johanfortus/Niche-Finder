f9006dd1626388779d64d13bba4d365ef1ab1c99
johanfortus <fortusjohan@gmail.com>
2024-11-07 15:26:50 -0500

    have tag section only show when apriori algorithm tab is active

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ let tabOne = document.querySelector(".tab-one");
 let tabTwo = document.querySelector(".tab-two");
 
 let tagInput = document.querySelector(".tag-input");
+let tagHead = document.querySelector(".tag-header");
+let tagField = document.querySelector(".tag-field");
 
 tabOne.addEventListener("click", (e) => {
     e.preventDefault();
     if(tabOne.classList[1] !== "active") {
         tabOne.classList.toggle("active");
         tabTwo.classList.toggle("active");
+
+        tagHead.classList.toggle("tag-section-invisible");
+        tagField.classList.toggle("tag-section-invisible");
+
         algoTitle.innerText = "K-3 Means Search Algorithm";
         algoDesc.innerText = "k-means clustering is a method of vector quantization, originally from signal processing, that aims to partition n observations into k clusters";
     }
@@ -22,6 +28,11 @@ tabTwo.addEventListener("click", (e) => {
     if(tabTwo.classList[1] !== "active") {
         tabTwo.classList.toggle("active");
         tabOne.classList.toggle("active");
+
+        tagHead.classList.toggle("tag-section-invisible");
+        tagField.classList.toggle("tag-section-invisible");
+
+
         algoTitle.innerText = "Apriori Algorithm";
         algoDesc.innerText = "A machine learning algorithm to identify relationships between items by identifying frequent itemsets.";
     }
