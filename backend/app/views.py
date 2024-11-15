d838f10560411a92751fd2f3992fc1ae7320a846
johanfortus <fortusjohan@gmail.com>
2024-11-15 02:53:49 -0500

    Refactored frontend as flask templates and handled form data using request.form

--- backend/app/views.py
+++ backend/app/views.py
@@ -4,8 +4,13 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home_page():
-    return
+    return render_template('home.html')
 
 @views.route('/result', methods = ['POST'])
 def result_page():
-    return
+    start_date = request.form['start-date']
+    end_date = request.form['end-date']
+    country = request.form['country']
+    engagement = request.form['engagement']
+
+    return render_template('result.html' , start_date=start_date, end_date=end_date, country=country, engagement=engagement)
