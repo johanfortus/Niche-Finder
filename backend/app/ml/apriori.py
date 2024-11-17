c7d49dbdd041f317b3037b1e2173b25e0190005a
johanfortus <fortusjohan@gmail.com>
2024-11-16 21:29:01 -0500

    added mlxtend imports and pandas for apriori algorithm

--- backend/app/ml/apriori.py
+++ backend/app/ml/apriori.py
@@ -1,3 +1,6 @@
+from mlxtend.frequent_patterns import apriori, association_rules
+import pandas as pd
+
 def perform_apriori(start_date, end_date, country, engagement, tags):
     print('PERFORMING APRIORI')
     print('------------------')
