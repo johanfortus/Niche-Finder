2d3310f3014a14046d4808e628ec428512a2ecfc
johanfortus <fortusjohan@gmail.com>
2024-11-17 02:10:15 -0500

    added simulated data to both kmeans.py and apriori.py

--- backend/app/ml/apriori.py
+++ backend/app/ml/apriori.py
@@ -4,3 +4,29 @@ import pandas as pd
 def perform_apriori(start_date, end_date, country, engagement, tags):
     print('PERFORMING APRIORI')
     print('------------------')
+
+
+# SIMULATED DATA
+'''
+simulated_data = {
+    'searchType': 'apriori',
+    'dateRange': {'start': '2024-11-01', 'end': '2024-11-17'},
+    'country': 'Spain',
+    'engagement': '72',
+    'tags': ['sports', 'soccer']
+}
+
+sim_start_date = simulated_data['dateRange']['start']
+sim_end_date = simulated_data['dateRange']['end']
+sim_country = simulated_data['country']
+sim_engagement = simulated_data['engagement']
+sim_tags = simulated_data['tags']
+
+print(f'Start Date: {sim_start_date}')
+print(f'End Date: {sim_end_date}')
+print(f'Country: {sim_country}')
+print(f'Engagement: {sim_engagement}')
+print(f'Tags: {sim_tags}')
+
+print(perform_apriori(sim_start_date, sim_end_date, sim_country, sim_engagement, sim_tags)) 
+'''
--- backend/app/ml/kmeans.py
+++ backend/app/ml/kmeans.py
@@ -1,9 +1,28 @@
 from sklearn.cluster import KMeans
 import pandas as pd
 
+simulated_data = {
+    'searchType': 'k-means',
+    'dateRange': {'start': '2024-11-01', 'end': '2024-11-17'},
+    'country': 'Spain',
+    'engagement': '72',
+    'tags': ['sports', 'soccer']
+}
+
+sim_start_date = simulated_data['dateRange']['start']
+sim_end_date = simulated_data['dateRange']['end']
+sim_country = simulated_data['country']
+sim_engagement = simulated_data['engagement']
+
+print(f'Start Date: {sim_start_date}')
+print(f'End Date: {sim_end_date}')
+print(f'Country: {sim_country}')
+print(f'Engagement: {sim_engagement}')
+
 def perform_kmeans(start_date, end_date, country, engagement):
     print('PERFORMING K-MEANS')
     print('------------------')
 
 
 
+# print(perform_kmeans(sim_start_date, sim_end_date, sim_country, sim_engagement))
