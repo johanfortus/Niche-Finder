1bf7a9d94f08e738e79c959bc366c2b0ab545e3c
johanfortus <fortusjohan@gmail.com>
2024-12-02 23:51:00 -0500

    Added grid lines, ticks, and labels to scatter plot

--- backend/app/static/js/result.js
+++ backend/app/static/js/result.js
@@ -23,6 +23,7 @@ function resultPage(scatterData) {
 
 }
 
+// Used Scatter Plot Template - https://d3-graph-gallery.com/graph/custom_theme.html
 function createScatterPlot(scatterData){
     console.log("CREATING SCATTER PLOT");
     console.log(scatterData);
@@ -46,4 +47,48 @@ function createScatterPlot(scatterData){
         .attr("width", height)
         .style("fill", "EBEBEB")
 
+    d3.json(scatterData, function(data) {
+
+        // X axis
+        var x = d3.scaleLinear()
+            .domain([4*0.95, 8*1.001])
+            .range([ 0, width ])
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).tickSize(-height*1.3).ticks(10))
+            .select(".domain").remove()
+        
+        // Y axis
+        var y = d3.scaleLinear()
+            .domain([-0.001, 9*1.01])
+            .range([ height, 0])
+            .nice()
+        svg.append("g")
+            .call(d3.axisLeft(y).tickSize(-width*1.3).ticks(7))
+            .select(".domain").remove()
+        
+        // Customization
+        svg.selectAll(".tick line")
+            .attr("stroke", "lightgray")
+
+        // X axis label
+        svg.append("text")
+            .attr("text-anchor", "end")
+            .attr("x", width/2 + margin.left)
+            .attr("y", height + margin.top + 20)
+            .text("View Count");
+        
+        // Y axis label
+        svg.append("text")
+            .attr("text-anchor", "end")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -margin.left + 20)
+            .attr("x", -margin.top - height/2 + 20)
+            .text("Like Count")
+        
+        // Color scale
+        var color = d3.scaleOrdinal()
+            .domain(["setosa", "versicolor", "virginica" ])
+            .range([ "#F8766D", "#00BA38", "#619CFF"])
+    })
 }
